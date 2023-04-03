77d5e38062587ef60a9b08d780cd216ea084fbc5
liushun <1743760069@qq.com>
2023-04-03 12:34:38 +0800

    feat:第三十六组测试，正确性测试完成

--- src/test/task/Task.java
+++ src/test/task/Task.java
@@ -9,10 +9,17 @@ import java.util.ArrayList;
 
 public class Task {
   public static void main(String[] args) {
+    correctnessTest();
+  }
+
+  /**
+   * 正确性测试
+   */
+  public static void correctnessTest() {
     MainEntry mainEntry = new MainEntry();
-    ArrayList<Graph> targetGraph = new BuildGraph().buildTargetGraph(35, 40);
+    ArrayList<Graph> targetGraph = new BuildGraph().buildTargetGraph(36, 40);
     for (Graph g : targetGraph) {
-      ArrayList<ArrayList<Solution>> executeResult = mainEntry.executeById(g, 34);
+      ArrayList<ArrayList<Solution>> executeResult = mainEntry.executeById(g, 35);
       System.out.println(executeResult);
     }
   }
